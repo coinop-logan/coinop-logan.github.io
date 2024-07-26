e84027797e6a0c695e7f66dd80e2ddfb8b9b94f8
coinop-logan <coinop.logan@gmail.com>
2024-07-26 16:06:55 +0300

    some scaffolding

---
+++ src/Config.elm
@@ -0,0 +1,13 @@
+module Config exposing (..)
+
+import Element
+
+
+bgColor : Element.Color
+bgColor =
+    Element.rgb 0.1 0.1 0.1
+
+
+defaultFontColor : Element.Color
+defaultFontColor =
+    Element.rgb 1 1 1
--- src/View.elm
+++ src/View.elm
@@ -1,7 +1,10 @@
 module View exposing (..)
 
 import Browser
+import Config
 import Element exposing (Attribute, Element)
+import Element.Background as Background
+import Element.Font as Font
 import Responsive exposing (DisplayProfile)
 import Types exposing (..)
 
@@ -47,4 +50,58 @@ viewLoadingMessage =
 
 view : LoadedModel -> Element Msg
 view model =
-    Element.text "hi"
+    let
+        dProfile =
+            model.dProfile
+    in
+    Element.el
+        [ Element.width Element.fill
+        , Element.height Element.fill
+        , Background.color Config.bgColor
+        , Font.color Config.defaultFontColor
+        ]
+    <|
+        Element.column
+            [ Element.width Element.fill
+            , Element.spacing 15
+            , Element.padding 30
+            ]
+            [ headerElement dProfile
+            , bodyElement dProfile
+            ]
+
+
+headerElement : DisplayProfile -> Element Msg
+headerElement dProfile =
+    Element.column
+        [ Element.centerX
+        , Element.spacing 10
+        ]
+        [ nameElement dProfile
+        , summaryElement dProfile
+        ]
+
+
+nameElement : DisplayProfile -> Element Msg
+nameElement dProfile =
+    Element.el
+        [ Element.centerX
+        , Font.size 50
+        ]
+    <|
+        Element.text "Logan Brutsche"
+
+
+summaryElement : DisplayProfile -> Element Msg
+summaryElement dProfile =
+    Element.el
+        [ Element.centerX
+        , Font.size 18
+        ]
+    <|
+        Element.text "summary here summary here summary here summary here "
+
+
+bodyElement : DisplayProfile -> Element Msg
+bodyElement dProfile =
+    Element.text "body"
